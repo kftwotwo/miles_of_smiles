7e422c7c56ff9dc524ec38ed5c6cd79a88758b48
Kevin Finley <kftwotwo@gmail.com>
2017-02-17 08:58:06 -0800

    add users model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :models
+  devise_for :users
 end
--- db/migrate/20170217164151_devise_create_users.rb
+++ db/migrate/20170217164151_devise_create_users.rb
@@ -1,7 +1,9 @@
-class DeviseCreateModels < ActiveRecord::Migration[5.0]
+class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :models do |t|
-      t.string :email,              null: false, default: ""
+    create_table :users do |t|
+      t.string :email,              null: false, default: "john@example.com"
+      t.string :first_name
+      t.string :last_name
       t.string :encrypted_password, null: false, default: ""
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -14,7 +16,7 @@ class DeviseCreateModels < ActiveRecord::Migration[5.0]
       t.timestamps null: false
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
