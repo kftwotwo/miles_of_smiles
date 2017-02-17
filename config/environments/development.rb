63bd70b3f90c8c6340714a370a025ea38667c996
Kevin Finley <kftwotwo@gmail.com>
2017-02-17 08:02:32 -0800

    setup the gem devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
